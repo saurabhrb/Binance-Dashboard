e06bf653737d218b53807fe8d3c51bb469705ba2
saurabhrb <saurabhbadenkalat@gmail.com>
2021-08-01 05:10:02 +0000

    clean up

--- main.py
+++ main.py
@@ -70,4 +70,4 @@ config = configparser.ConfigParser()
 config.read('keys.cfg')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='localhost', port=8080)
+    socketio.run(app, debug=False, host='0.0.0.0', port=8080)
